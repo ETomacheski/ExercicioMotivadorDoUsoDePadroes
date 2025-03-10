9b613cec78781ec4b2266957139adf8663a85ae0
Eduardo Tomacheski <tomacheski2001@gmail.com>
2025-03-10 20:58:19 +0000

    feat: create utilitario car

--- src/App.java
+++ src/App.java
@@ -3,9 +3,10 @@ public class App {
 
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
         Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
+        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5, 70);
 
         System.out.println("Tipos de veiculos:");
-        System.out.println(esportivo);
+        System.out.println(utilitario);
 
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro basico com gasolina");
